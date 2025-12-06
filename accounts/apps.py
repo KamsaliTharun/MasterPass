d39641bc721a512a63f0631f3ac5e4cb68d133a6
Kamsali Tharun <kasmalitharun981@gmail.com>
2025-12-06 23:55:12 +0530

    adding admin user

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,19 @@
 from django.apps import AppConfig
+from django.contrib.auth.models import User
 
 
 class AccountsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "accounts"
+
+
+
+
+
+class MyAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'accounts'
+
+    def ready(self):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', 'admin@example.com', 'Light#2020')
