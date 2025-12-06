aca72626c3a86985236ada3ec208c6c74ca0f55c
Kamsali Tharun <kasmalitharun981@gmail.com>
2025-12-07 00:03:36 +0530

    changed the import startment place

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,5 +1,5 @@
-from django.apps import AppConfig
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+# from django.apps import AppConfig
 
 
 # class AccountsConfig(AppConfig):
@@ -9,16 +9,25 @@ from django.contrib.auth.models import User
 
 
 
+from django.apps import AppConfig
+
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
 
     def ready(self):
         try:
+            # Import here, so apps are fully loaded
             from django.contrib.auth import get_user_model
             User = get_user_model()
-            if not User.objects.filter(username='itadmin').exists():
-                User.objects.create_superuser('itadmin', 'itadmin@masterpass.com', 'Master@123')
-        except Exception as e:
-            # Ignore errors during initial migrations
+
+            # Create superuser only if it doesn't exist
+            if not User.objects.filter(username='admin').exists():
+                User.objects.create_superuser(
+                    username='admin',
+                    email='admin@example.com',
+                    password='YourStrongPassword123'
+                )
+        except Exception:
+            # Ignore errors during first migrations
             pass
