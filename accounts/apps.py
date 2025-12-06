fb8a851498bd37fe891d8a2c9dedc8b074fbd169
Kamsali Tharun <kasmalitharun981@gmail.com>
2025-12-07 00:12:41 +0530

    removed ready method from app.py

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,33 +1,33 @@
-# from django.contrib.auth.models import User
-# from django.apps import AppConfig
-
-
-# class AccountsConfig(AppConfig):
-#     default_auto_field = "django.db.models.BigAutoField"
-#     name = "accounts"
-
-
-
-
 from django.apps import AppConfig
 
+
 class AccountsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accounts'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "accounts"
 
-    def ready(self):
-        try:
-            # Import here, so apps are fully loaded
-            from django.contrib.auth import get_user_model
-            User = get_user_model()
 
-            # Create superuser only if it doesn't exist
-            if not User.objects.filter(username='admin').exists():
-                User.objects.create_superuser(
-                    username='admin',
-                    email='admin@example.com',
-                    password='YourStrongPassword123'
-                )
-        except Exception:
-            # Ignore errors during first migrations
-            pass
+
+
+# from django.apps import AppConfig
+
+# class AccountsConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'accounts'
+
+#     def ready(self):
+#         try:
+#             # Import here, so apps are fully loaded
+#             from django.contrib.auth import get_user_model
+#             User = get_user_model()
+
+#             # Create superuser only if it doesn't exist
+#             if not User.objects.filter(username='admin').exists():
+#                 User.objects.create_superuser(
+#                     username='Tharun',
+#                     # email='admin@example.com',
+#                     email='Tharun@masterpass.com',
+#                     password='Light#2020'
+#                 )
+#         except Exception:
+#             # Ignore errors during first migrations
+#             pass
